523dd2e18f6cf699c315e1f8ce03853aa4811dae
ActuallyDoc <actuallydoc@gmail.com>
2022-07-24 22:37:29 +0200

    White text under the picture.
    Was black before so it was hard to see

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -25,7 +25,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
         <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
           {result.title || result.original_name}
         </h2>
-        <p className="flex items-center opacity-0 group-hover:opacity-100 ">
+        <p className="flex items-center opacity-0 group-hover:opacity-100 text-white ">
           {result.media_type && `${result.media_type} •`}{" "}
           {result.release_date || result.first_air_date} •{" "}
           <ThumbUpIcon className="h-5 mx-2 text-white" /> {result.vote_count}
